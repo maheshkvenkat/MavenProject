8f686c4bfa5eaeaa38823fd65210d81a17abc8e0
maheshkvenkat <venkatmahesh808@gmail.com>
2024-03-19 19:22:47 +0530

    Maven Project

---
+++ src/main/java/sanityTesting/NewCustomerCreation.java
@@ -0,0 +1,12 @@
+package sanityTesting;
+
+public class NewCustomerCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is  new Customer creation task ");
+
+	}
+
+}
